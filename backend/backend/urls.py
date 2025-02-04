06ce4749ccc68c25c1fefd0f40d05f6de0d7eced
root <root@DESKTOP-0PA1CJG.>
2025-02-05 00:36:09 +0300

    Move Procfile to root

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), # Add the api app's URL patterns to the project's URL patterns
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
