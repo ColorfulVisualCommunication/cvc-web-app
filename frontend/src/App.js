bfe318a8a5334be1afd7d675839f291a1967dd16
root <root@DESKTOP-0PA1CJG.>
2025-02-06 10:06:17 +0300

    reconfigured and moved react build files to backend/static

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/")
-      .then(response => setMessage(response.data.message))
-      .catch(error => console.error(error));
-  }, []);
-
   return (
-    <div>
-      <h1>React Frontend</h1>
-      <p>{message}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
